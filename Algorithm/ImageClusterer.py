0a00e0b65d02945e4bf59a292fb05cf9fb72eb57
Janne Gröndahl <janne.grondahl@aalto.fi>
2015-03-18 12:59:14 +0200

    Update image clustering

--- Algorithm/ImageClusterer.py
+++ Algorithm/ImageClusterer.py
@@ -1,49 +1,124 @@
 import numpy as np
 import cv2
-import os, sys, argparse
-from os import listdir
-from os.path import isfile, join
 import json
+from Utilities import *
 
-def extract_features(img):
+import os
+import sys
+
+'''from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn.datasets.samples_generator import make_blobs
+import matplotlib.pyplot as plt
+from itertools import cycle'''
+
+#sys.path.append(os.path.join('vocpy-master', 'tools'))
+#import generatecodebook
+
+class Image:
+  def __init__(self, image_path, metadata_path, features):
+    self.image_path = image_path
+    self.load_metadata(metadata_path)
+    self.features = features
+    
+  def load_metadata(self, metadata_path):
+    with open(metadata_path, 'r') as f:
+      self.metadata = json.load(f)
+
+def image_distance(img1, img2):
   pass
 
-# This is the actual clustering algorithm
-def cluster_images(image_paths, metadata):
-  print "Extracting visual features"
-  features = []
-  for path in image_paths:
-    img = cv2.imread(path, cv2.IMREAD_COLOR)
-    features.append(extract_features(img))
-  print "Clustering"
+def cluster_images(images):
+  #images = images[:30]
+  descriptors = []
+  for image in images[:2000]:
+    try:
+      img = cv2.imread(image.image_path, cv2.IMREAD_COLOR)
+      surf = cv2.SURF(400)
+      kp, des = surf.detectAndCompute(img, None)
+    except:
+      print "fuufuu"
+      break
+    try:
+      image.features = (kp, des)
+      descriptors.extend(des)
+    except:
+      print "failed to add descriptors", len(kp)
+    #print des
+    #print '---'
+    
+  # Cluster descriptors for codebook  (borrowed from codebook.py)
+  from sklearn.cluster import MiniBatchKMeans
+  print "descriptors:", len(descriptors)
+  return
+  #n_clusters = len(descriptors) / 400
+  n_clusters = 1000
+  print "codes:", n_clusters
+  mbk = MiniBatchKMeans(init='k-means++', n_clusters=n_clusters,
+      batch_size=n_clusters, n_init=3, max_iter=50,
+      max_no_improvement=3, verbose=0, compute_labels=False)
+  mbk.fit(descriptors)
+  codebook = mbk.cluster_centers_
+  print "codebook:", codebook
+  
+  '''visual_distances = np.zeros((len(images), len(images)))
+  for i in range(len(images)):
+    for j in range(i+1, len(images)):
+      d = 0.0
+      visual_distances[i, j] = d'''
+  
+'''# This is the actual clustering algorithm
+def cluster_images_test(images):
+  print "Clustering images"
+  centers = [[1,1], [-1,-1], [1,-1]]
+  
+  X = [[0.0,0.0],[0.2,0.1],[1.0,1.0],[-2.0,-0.5],[-1.0,-2.0]]
+  X = []
+  for img in images:
+    gps = img.metadata['gps']
+    X.append([float(gps[0]), float(gps[1])])
+  X = np.array(X)
+  print X
+  
+  #X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=0.6)
+  print X
+  #bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
+  #print bandwidth
+  bandwidth = 0.5
+  ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+  ms.fit(X)
+  labels = ms.labels_
+  cluster_centers = ms.cluster_centers_
+  labels_unique = np.unique(labels)
+  n_clusters_ = len(labels_unique)
+  print "number of estimated clusters:", n_clusters_
+  
+  plt.figure(1)
+  plt.clf()
+  colors = cycle('bgrcmyk')
+  for k, col in zip(range(n_clusters_), colors):
+    my_members = labels==k
+    cluster_center = cluster_centers[k]
+    plt.plot(X[my_members,0], X[my_members,1], col+'.')
+    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=14)
+  plt.show()'''
   
 
-def get_image_paths(folder):
-    all_file_names = [f for f in listdir(folder) if isfile(join(folder, f))]
-    all_file_names = [f for f in all_file_names if f != 'ratings.txt']
-    all_file_names.sort()
-    files = [folder + all_file_names[i].split('.')[0] for i in range(0, len(all_file_names), 2)]
-    images = [f + '.jpg' for f in files]
-    metadata = [f + '.txt' for f in files]
-    return (images, metadata)
+def main():
+  folder = get_folder_argument()
+  base_folder = '../' + folder + '/'
   
-def main(argv):
-  # Parse script parameters
-  parser = argparse.ArgumentParser(description='This script clusters images of same views')
-  parser.add_argument('-f', '--folder_name', help='Image folder name', required=True)
-  args = parser.parse_args()
-  folder = '../' + args.folder_name + '/'
+  # Load features
+  all_features = load_features(base_folder)
   
   # Read metadata from files
-  (images, metadata_paths) = get_image_paths(folder)
-  metadata = []
-  for path in metadata_paths:
-    with open(path, 'r') as f:
-      md = json.load(f)
-      metadata.append(md)
+  (image_paths, metadata_paths) = get_image_paths(base_folder)
+  images = []
+  for i in range(len(image_paths)):
+    features = ['todo']
+    images.append(Image(image_paths[i], metadata_paths[i], features))
   
   # Start the main algorithm
-  cluster_images(images, metadata)
+  cluster_images(images)
 
 if __name__ == '__main__':
-  main(sys.argv[1:])
+  main()
