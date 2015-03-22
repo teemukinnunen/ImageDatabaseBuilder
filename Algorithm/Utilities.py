86551fee60276afb2e5766faaa65d9c657412f03
Janne Gröndahl <janne.grondahl@aalto.fi>
2015-03-22 21:17:29 +0200

    Update utilities

--- Algorithm/Utilities.py
+++ Algorithm/Utilities.py
@@ -2,7 +2,29 @@
 from os import listdir
 from os.path import isfile, join
 import os, sys, argparse
+import shutil
+import os
 
+def copy_images(input_folder, output_folder, img_paths, md_paths):
+  if input_folder == output_folder:
+    print "input folder can't be same as output folder!"
+    return
+  if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+  print "Copying {} photos".format(len(img_paths))
+  for i in range(len(img_paths)):
+    shutil.copy2(input_folder + img_paths[i], output_folder + img_paths[i])
+    shutil.copy2(input_folder + md_paths[i], output_folder + md_paths[i])
+
+def get_filename(path): # probably doesn't work on other than Windows
+  if '/' not in path:
+    return path
+  else:
+    return path.split('/')[-1]
+
+def get_folder(path):
+  return path[:path.rfind('/')+1]
+  
 def get_folder_argument():
   parser = argparse.ArgumentParser(description='This script clusters images of same views')
   parser.add_argument('-f', '--folder_name', help='Image folder name', required=True)
@@ -17,13 +39,3 @@ def get_image_paths(folder):
     images = [f + '.jpg' for f in files]
     metadata = [f + '.txt' for f in files]
     return (images, metadata)
-
-def save_features(folder_name, features):
-  with open('./features/' + folder_name + '.txt', 'w') as f:
-    f.write("asd\n")
-  
-
-def load_features(folder_name):
-  features = []
-  return features
-  
--- utilities.py
+++ utilities.py
@@ -11,3 +11,12 @@ def copy_images(input_folder, output_folder, img_paths, md_paths):
   for i in range(len(img_paths)):
     shutil.copy2(input_folder + img_paths[i], output_folder + img_paths[i])
     shutil.copy2(input_folder + md_paths[i], output_folder + md_paths[i])
+
+def get_filename(path): # probably doesn't work on other than Windows
+  if '/' not in path:
+    return path
+  else:
+    return path.split('/')[-1]
+
+def get_folder(path):
+  return path[:path.rfind('/')+1]
